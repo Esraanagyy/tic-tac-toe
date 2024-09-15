110570ead34abff4b6771cd72b88f1a740de3165
Esraa Nagy <en1228@fayoum.edu.eg>
2024-09-15 07:23:00 +0300

    * adding game view

---
+++ lib/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static Color primaryColor = const Color(0xFF5C4033);
+  static Color secondaryColor = const Color(0xFFFFFFC5);
+  static Color accentColor  = const Color(0xFFC4A484);
+
+
+}
